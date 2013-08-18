12823ad2f7ce8c226e0aaa4796f97e6c8c6c625c
walis85300 <walojose46@gmail.com>
2013-08-17 22:28:31 -0430

    Actualizacion
    se creo la pagina para las publicaciones

--- static/js/codigo.js
+++ static/js/codigo.js
@@ -3,16 +3,11 @@ $('#home').on('click', function(){
 	$('#menuMoviles').css({
 
 		"display" : "block",
-		"-webkit-transition":"all 1s ease",
-		"-moz-transition":"all 1s ease",
-		"-o-transition":"all 1s ease",
-		"transition":"all 1s ease"
-
 	});
 
 });
 
-$('#cerrar figcaption').on('click',function(){
+$('#cerrar').on('click',function(){
 
 	$('#menuMoviles').css({
 		"display" : "none"
